2f615b3e3c02352259f7bb4fcc91980a9f6c501b
nicolasmederoc <nicolas.medero@wolox.com.ar>
2019-06-11 11:42:48 -0300

    Card de Login Frontend 1, únicamente componentes.

---
+++ app/src/main/java/ar/com/wolox/android/example/ui/LoginActivity.java
@@ -0,0 +1,18 @@
+package ar.com.wolox.android.example.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import ar.com.wolox.android.R;
+
+/**
+ * LoginActivity
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
