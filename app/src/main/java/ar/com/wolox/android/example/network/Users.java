4dc1daae7de171b4c42ef3e18065fd0a8ed0a3da
nicolasmederoc <nicolas.medero@wolox.com.ar>
2019-06-19 17:28:45 -0300

    Cleaning imports and variables from Users.java

--- app/src/main/java/ar/com/wolox/android/example/network/Users.java
+++ app/src/main/java/ar/com/wolox/android/example/network/Users.java
@@ -1,51 +1,28 @@
 package ar.com.wolox.android.example.network;
 
-import com.google.gson.annotations.Expose;
-import com.google.gson.annotations.SerializedName;
-
 /**
  * User class for the REST API
  */
 
 public class Users {
-    @SerializedName("id")
-    @Expose
     private String id;
 
-    @SerializedName("username")
-    @Expose
     private String username;
 
-    @SerializedName("email")
-    @Expose
     private String email;
 
-    @SerializedName("password")
-    @Expose
     private String password;
 
-    @SerializedName("picture")
-    @Expose
     private String picture;
 
-    @SerializedName("cover")
-    @Expose
     private String cover;
 
-    @SerializedName("description")
-    @Expose
     private String description;
 
-    @SerializedName("location")
-    @Expose
     private String location;
 
-    @SerializedName("name")
-    @Expose
     private String name;
 
-    @SerializedName("phone")
-    @Expose
     private String phone;
 
     public String getEmail() {
