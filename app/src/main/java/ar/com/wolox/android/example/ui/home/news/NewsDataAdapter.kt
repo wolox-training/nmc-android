5bb07939380bbe4ec852570a3e64e2a5e95ac35d
Nicolás Medero C <nicolas.medero@wolox.com.ar>
2019-06-27 15:33:00 -0300

    Creating NewsDataAdapter to use with the RecyclerView

--- app/src/main/java/ar/com/wolox/android/example/ui/home/news/NewsDataAdapter.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/home/news/NewsDataAdapter.kt
@@ -4,20 +4,19 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NewsDataAdapter (private val newsDataAdapter: Array<String>) : RecyclerView.Adapter<NewsDataAdapter.NewsViewHolder>() {
+class NewsDataAdapter(private val newsDataAdapter: Array<String>) : RecyclerView.Adapter<NewsDataAdapter.NewsViewHolder>() {
 
     class NewsViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsDataAdapter.NewsViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onBindViewHolder(holder: NewsDataAdapter.NewsViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
-
 }
