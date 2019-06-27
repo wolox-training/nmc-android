a5ba5935769672b6231a41d711a91ca0069e354d
Nicolás Medero C <nicolas.medero@wolox.com.ar>
2019-06-27 12:50:30 -0300

    Added a Tab Listener

--- app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import ar.com.wolox.android.example.ui.home.profile.ProfileFragment
 import ar.com.wolox.wolmo.core.adapter.viewpager.SimpleFragmentPagerAdapter
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
 import ar.com.wolox.wolmo.core.presenter.BasePresenter
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_home.*
 import javax.inject.Inject
 
@@ -25,6 +26,9 @@ class HomeFragment @Inject constructor() : WolmoFragment<BasePresenter<Any>>() {
     }
 
     override fun init() {
+        vToolbarFragmentHome.setLogo(R.drawable.wolox_logo)
+        vToolbarFragmentHome.setTitle(R.string.example_wolox)
+
         vTabLayoutFragmentHome.setupWithViewPager(vViewPagerFragmentHome)
 
         fragmentHomePagerAdapter = SimpleFragmentPagerAdapter(childFragmentManager)
@@ -32,7 +36,34 @@ class HomeFragment @Inject constructor() : WolmoFragment<BasePresenter<Any>>() {
                 Pair(page1news, "News"),
                 Pair(page2profile, "Profile")
         )
-
         vViewPagerFragmentHome.adapter = fragmentHomePagerAdapter
+
+        vTabLayoutFragmentHome.getTabAt(0)!!.setIcon(R.drawable.ic_news_list_on)
+        vTabLayoutFragmentHome.getTabAt(1)!!.setIcon(R.drawable.ic_profile_off)
+
+        onTabView()
+    }
+
+    private fun onTabView() {
+        vViewPagerFragmentHome.addOnPageChangeListener(
+                TabLayout.TabLayoutOnPageChangeListener(vTabLayoutFragmentHome))
+        vTabLayoutFragmentHome.addOnTabSelectedListener(object :
+            TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                if (vTabLayoutFragmentHome.getTabAt(0)?.isSelected!!) {
+                    vTabLayoutFragmentHome.getTabAt(0)!!.setIcon(R.drawable.ic_news_list_on)
+                    vTabLayoutFragmentHome.getTabAt(1)!!.setIcon(R.drawable.ic_profile_off)
+                } else {
+                    vTabLayoutFragmentHome.getTabAt(0)!!.setIcon(R.drawable.ic_news_list_off)
+                    vTabLayoutFragmentHome.getTabAt(1)!!.setIcon(R.drawable.ic_profile_on)
+                }
+            }
+
+            override fun onTabReselected(p0: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+            }
+        })
     }
 }
