0bfe89081cb2f5a7265c464c557d9f21897b71eb
Nicolás Medero C <nicolas.medero@wolox.com.ar>
2019-07-05 15:49:43 -0300

    Custom Toolbar improvements.

--- app/src/main/java/ar/com/wolox/android/example/ui/customView/CustomToolbar.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/customView/CustomToolbar.kt
@@ -12,19 +12,18 @@ class CustomToolbar @JvmOverloads constructor(
     defStyleAttr: Int = R.attr.toolbarStyle
 ) : Toolbar(context, attrs, defStyleAttr) {
 
-    private val woloxIcon = R.drawable.wolox_logo
-
     init {
         setBackgroundColor(Color.TRANSPARENT)
         setLogo(woloxIcon)
-        setTitle(R.string.example_wolox)
+        title = woloxTitle
     }
 
-    fun setHomeTitle() {
-        setTitle(R.string.toolbar_wolox_home)
+    fun setCustomTitle(customTitle: String) {
+        title = "$woloxTitle - $customTitle"
     }
 
-    fun setNewsCreationTitle() {
-        setTitle(R.string.toolbar_wolox_news_creation)
+    companion object {
+        private val woloxIcon = R.drawable.wolox_logo
+        private val woloxTitle = "Wolox"
     }
 }
