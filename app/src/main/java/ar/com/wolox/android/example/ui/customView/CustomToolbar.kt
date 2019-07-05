50bd9e3592f4eaabb2717931a40cc3020fab9652
Nicolás Medero C <nicolas.medero@wolox.com.ar>
2019-07-05 11:43:20 -0300

    Implementing CustomToolbar

--- app/src/main/java/ar/com/wolox/android/example/ui/customView/CustomToolbar.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/customView/CustomToolbar.kt
@@ -1,48 +1,30 @@
 package ar.com.wolox.android.example.ui.customView
 
 import android.content.Context
-import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
-import android.os.Build
 import android.util.AttributeSet
-import android.widget.Toolbar
-import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.Toolbar
 import ar.com.wolox.android.R
 
-@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 class CustomToolbar @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
+    defStyleAttr: Int = R.attr.toolbarStyle
 ) : Toolbar(context, attrs, defStyleAttr) {
 
-    val newPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    lateinit var newRect: Rect
-    private var squareColor: Int = 0
+    private val woloxIcon = R.drawable.wolox_logo
 
-    fun init(set: AttributeSet?) {
-        if (set == null) return
-
-        val typedArray = context.obtainStyledAttributes(set, R.styleable.CustomToolbar)
-        squareColor = typedArray.getColor(R.styleable.CustomToolbar_CustomToolbar_color, Color.BLACK)
-        newPaint.color = squareColor
-        typedArray.recycle()
+    init {
+        setBackgroundColor(Color.TRANSPARENT)
+        setLogo(woloxIcon)
+        setTitle(R.string.example_wolox)
     }
 
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        newRect = Rect()
-        newPaint.color = Color.BLUE
-        setCustomToolbarDimensions()
-        canvas.drawRect(newRect, newPaint)
+    fun setHomeTitle() {
+        setTitle(R.string.toolbar_wolox_home)
     }
 
-    private fun setCustomToolbarDimensions() {
-        newRect.top = 0
-        newRect.bottom = R.dimen.spacing_largest_extra
-        newRect.left = 0
-        newRect.right = width
+    fun setNewsCreationTitle() {
+        setTitle(R.string.toolbar_wolox_news_creation)
     }
 }
