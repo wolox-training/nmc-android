8d99c544ea082391531fcde71a64b48647c153ba
Nicolás Medero C <nicolas.medero@wolox.com.ar>
2019-08-08 18:38:16 -0300

    Simplifying all in one view

--- app/src/main/java/ar/com/wolox/android/example/ui/home/profile/ProfileFragment.kt
+++ app/src/main/java/ar/com/wolox/android/example/ui/home/profile/ProfileFragment.kt
@@ -2,12 +2,11 @@ package ar.com.wolox.android.example.ui.home.profile
 
 import android.app.AlertDialog
 import android.widget.Toast
-import androidx.core.util.Pair
-import androidx.viewpager.widget.ViewPager
+import androidx.recyclerview.widget.LinearLayoutManager
 import ar.com.wolox.android.R
-import ar.com.wolox.android.example.ui.home.profile.turnOnCooktop.TurnOnCooktop
+import ar.com.wolox.android.example.network.Item
 import ar.com.wolox.android.example.utils.onClickListener
-import ar.com.wolox.wolmo.core.adapter.viewpager.SimpleFragmentPagerAdapter
+
 import ar.com.wolox.wolmo.core.fragment.WolmoFragment
 import ar.com.wolox.wolmo.core.presenter.BasePresenter
 import kotlinx.android.synthetic.main.fragment_profile.*
@@ -15,10 +14,8 @@ import javax.inject.Inject
 
 class ProfileFragment @Inject constructor() : WolmoFragment<BasePresenter<Any>>(), IProfileView {
 
-    @Inject lateinit var dietFragment: Diet
-    @Inject lateinit var allergicFragment: Allergic
-    @Inject lateinit var turnOnCooktop: TurnOnCooktop
-    private lateinit var fragmentProfileAdapter: SimpleFragmentPagerAdapter
+    private var items = ArrayList<Item>()
+    private val itemAdapter = ItemViewAdapter(items)
 
     override fun layout(): Int {
         return R.layout.fragment_profile
@@ -26,45 +23,22 @@ class ProfileFragment @Inject constructor() : WolmoFragment<BasePresenter<Any>>(
 
     override fun init() {
 
+        loadItems()
+
         val avoidDialog = AlertDialog.Builder(activity).apply {
             setMessage("En la sección Mi Perfil podrás completar las preferencias")
             setPositiveButton("Aceptar") { _, _ -> }
         }.create()
 
-        vTabLayoutProfile.setupWithViewPager(vViewPagerProfile)
-
-        fragmentProfileAdapter = SimpleFragmentPagerAdapter(childFragmentManager).apply {
-            addFragments(
-                    Pair(dietFragment, ""),
-                    Pair(allergicFragment, ""),
-                    Pair(turnOnCooktop, "")
-            )
-        }
-
-        vViewPagerProfile.apply {
-            adapter = fragmentProfileAdapter
-            offscreenPageLimit = VIEW_PAGER_TABS
-            addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
-                override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-                }
-
-                override fun onPageSelected(position: Int) {
-                }
-
-                override fun onPageScrollStateChanged(state: Int) {
-                }
-            })
+        vOnboardingRecyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = itemAdapter
         }
 
         avoid.onClickListener {
-            if (vViewPagerProfile.currentItem != 2) {
-                avoidDialog.show()
-                vViewPagerProfile.currentItem = 2
-            } else {
-            }
+            avoidDialog.show()
         }
-
-        turnOnCooktop.onContinueClick = { saveAllData() }
     }
 
     /**
@@ -73,6 +47,12 @@ class ProfileFragment @Inject constructor() : WolmoFragment<BasePresenter<Any>>(
      * */
     fun saveAllData() {
         Toast.makeText(context, "Continue touched", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun loadItems() {
+        for (i in 0..10) {
+            items.add(Item("Item $i", false))
+        }
     }
 
     companion object {
